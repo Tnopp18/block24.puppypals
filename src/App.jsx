868b79ba7f6000ec85b41bfab1fc071f48f2b96e
Thomas Noppenberger <thomas.lee.noppenberger@gmail.com>
2024-03-25 17:35:27 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {puppyList} from "./data.js"
+import './App.css'
+import {useState} from 'react'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find(pup => pup.id === featPupId);
+
+  return (
+    <>
+      <div id="root"> { 
+   puppies.map((puppy) => {
+     return (
+     <p onClick={() => setFeatPupId(puppy.id)} key = {puppy.id}>
+      {puppy.name}
+      </p>
+     );
+   })
+}
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      </div>
+    </>
+  )
+}
+
+export default App
